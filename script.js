16425f195f4c0c223a2f54681f66604e1556038b
Daryll M <djtm97@gmail.com>
2022-01-01 18:17:56 -0600

    add local storage to saving and loading saved tasks

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
-var currentDate = moment().format('MMMM Do YYYY');
-var currentHour = moment().hour();
+var currentDate = moment().format('MMMM Do YYYY'); //date to display in calendar
+var currentHour = moment().hour(); //military time in hours
+var savedText = {
+    "9": {
+        textDescription: ""
+    },
+    "10": {
+        textDescription: ""
+    },
+    "11": {
+        textDescription: ""
+    },
+    "12": {
+        textDescription: ""
+    },
+    "13": {
+        textDescription: ""
+    },
+    "14": {
+        textDescription: ""
+    },
+    "15": {
+        textDescription: ""
+    },
+    "16": {
+        textDescription: ""
+    },
+    "17": {
+        textDescription: ""
+    }
+}
 
 $("#currentDay").html(currentDate);
 console.log(currentHour);
 
 var addText = function() {
-
+    var textDescription = $(this).siblings(".description").val().trim();
+    var timeId = ($(this).attr("id")).split(":")[0];
+    console.log(timeId + ": " + textDescription);
+    savedText[timeId].textDescription = textDescription;
+    console.log(savedText)
+    localStorage.setItem("savedTasks", JSON.stringify(savedText));
 }
 
+var loadText = function() {
+    var savedTasks = localStorage.getItem("savedTasks");
+
+    if (savedTasks) {
+        savedTasks = JSON.parse(savedTasks);
+        savedText = savedTasks;
+        for (var i = 9; i < 18; i++) {
+            $("#" + i + " .description").val(savedText[i].textDescription);
+        }
+    }
+}
+
+// function to update classes based on time
 var checkTime = function() {
     var timeDiff = $(this).attr("id")-currentHour;
 
@@ -31,3 +78,6 @@ var checkTime = function() {
 //Event listeners
 $(".saveBtn").on("click", addText);
 $(".time-block").each(checkTime);
+
+//Call function upon load
+loadText();
